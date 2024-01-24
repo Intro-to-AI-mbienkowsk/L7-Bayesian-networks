9935ee5722c0dacbe0643bed3a93430121ad0939
maksym <01178511@pw.edu.pl>
2024-01-24 20:57:28 +0100

    the experiment part is done?? maybe should refactor later

--- src/experiments.py
+++ src/experiments.py
@@ -12,43 +12,64 @@ class BNRun:
 
 @dataclass
 class BNExperiment:
-    evidence_nodes: list[str]
-    node_of_interest: list[str]
+    """Represents a bayesian network experiment given the evidence (nodes with a single, set value),
+    list of nodes to permutate the values of and also pass as evidence to the network (to check how their values
+    affect the nodes of interest) and nodes of interest, the probabilities of which should be returned when running
+     the experiment"""
+    concrete_evidence: dict
+    nodes_to_permutate: list[str]
+    nodes_of_interest: list[str]
 
 
-def format_evidence(evidence: dict):
+def format_evidence(evidence: dict) -> list[list[dict]]:
     """Reformat the evidence dict to pomegranate's format"""
     return [[evidence.get(node.name, None) for node in network.states]]
 
 
-def conduct_experiment(exp: BNExperiment):
-    possible_evidence_node_vals = [enum_values_from_string[ev] for ev in exp.evidence_nodes]
+def create_runs(exp: BNExperiment) -> list[BNRun]:
+    """Create all the runs for the given experiments before running them"""
+    possible_evidence_node_vals = [enum_values_from_string[ev] for ev in exp.nodes_to_permutate]
     possible_evidence_combinations = list(product(*possible_evidence_node_vals))
+    runs = list()
+
     for combination in possible_evidence_combinations:
-        evidence = {exp.evidence_nodes[i]: combination[i] for i in range(len(exp.evidence_nodes))}
-        run = BNRun(evidence)
-        # todo: calculate values of nodes of interest
+        # single run with the network's evidence being constructed from conc_evidence and a permutation of nodes_to_iter
+        permutated_evidence = {
+            exp.nodes_to_permutate[i]: combination[i]
+            for i in range(len(exp.nodes_to_permutate))
+        }
+        final_evidence = {**exp.concrete_evidence, **permutated_evidence}
+        runs.append(BNRun(final_evidence))
+    return runs
 
 
-# Evidence
-evidence = {
-    "weather": Weather.HARSH.value,
-    "car_maintenance": Maintenance.POORLY_MAINTAINED.value,
-    "battery_age": BatteryAge.OLD.value,
-    "electrical_system": ElectricalSystem.FAULTY.value
-}
+def extract_relevant_nodes(exp: BNExperiment, network_output: list):
+    """Extracts from the network output a dictionary with keys being the node names
+    and values being their probability distributions only for the nodes relevant for
+    the experiment - named in the conc_evidence, nodes_to_permutate or nodes_of_interest"""
+    result = {**exp.concrete_evidence}
+    for node_name in exp.nodes_to_permutate + exp.nodes_of_interest:
+        node_idx = next(i for i, node in enumerate(network.states) if node.name == node_name)
+        # extract either a string - the state if the node is proof or the probs from a distribution object
+        result[node_name] = network_output[0][node_idx] \
+            if node_name in exp.nodes_to_permutate else network_output[0][node_idx].parameters[0]
+    return result
 
-# Format the evidence
-observations = [[evidence.get(node.name, None) for node in network.states]]
 
-# Query the network
-predictions = network.predict_proba(observations)
+def conduct_experiment(exp: BNExperiment) -> list[BNRun]:
+    """"Conduct an entire experiment, returning the list of runs with their results filled out"""
+    runs = create_runs(exp)
+    for run in runs:
+        formatted_evidence = format_evidence(run.evidence)
+        network_output = network.predict_proba(formatted_evidence)
+        run.results = extract_relevant_nodes(exp, network_output)
+    for run in runs:
+        print("------------------------\n")
+        print(run.results)
+    return runs
 
-sample_predictions = predictions[0]
 
-# Get specific node distribution (e.g., for 'engine_start')
-engine_start_index = next(i for i, node in enumerate(network.states) if node.name == "engine_start")
-engine_start_distribution = sample_predictions[engine_start_index]
+evidence_nodes = ["weather", "engine_noise"]
+nodes_of_interest = ["electrical_system"]
+conduct_experiment(BNExperiment({"battery_age": "Old"}, evidence_nodes, nodes_of_interest))
 
-# Print the distribution
-print(engine_start_distribution)
